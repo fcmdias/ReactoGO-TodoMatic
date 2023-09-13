8345c77bbeb5a19c3acaa83940f6770ad1907e6f
fcmdias <fernando.cesardias7@yahoo.com>
2023-09-13 23:23:15 +0100

    fixed expired token issue in the front-end

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,16 +2,28 @@ import React, { useEffect } from 'react';
 import './App.css';
 import Dashboard from './components/Dashboard';
 import { useDispatch } from 'react-redux';
-import { loginSuccessAction } from './features/auth/actions'; 
+import { loginSuccessAction, logoutUser } from './features/auth/actions'; 
 import Cookies from 'js-cookie';
 
 
 function App() {
   const dispatch = useDispatch();
   useEffect(() => {
+    const isTokenExpired = (token) => {
+      try {
+          const { exp } = JSON.parse(atob(token.split('.')[1]));
+          return exp < (Date.now() / 1000); // Convert current time to Unix timestamp
+      } catch (e) {
+          return true; // If there's an error, consider the token invalid
+      }
+  }
     const token = Cookies.get('auth_token');
     if (token) {
+      if (isTokenExpired(token)) {
+        dispatch(logoutUser);
+      } else {
         dispatch(loginSuccessAction(token));
+      }
     }
   }, [dispatch]);
 
