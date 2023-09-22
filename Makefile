03e08115f55eafa3eddf4cda73ea61977f0484d4
fcmdias <fernando.cesardias7@yahoo.com>
2023-09-22 20:09:07 +0100

    added code to filter tasks by category in the front-end

--- Makefile
+++ Makefile
@@ -4,3 +4,7 @@ up:
 reset: 
 	docker-compose down
 	docker-compose up --build
+
+rebuild-react:
+	docker-compose build react-app
+	docker-compose up -d react-app
--- react-app/src/features/tasks/List.js
+++ react-app/src/features/tasks/List.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchTasks } from './actions';
 import { fetchCategories } from '../categories/actions';
@@ -7,6 +7,12 @@ import TaskItem from './Item';  // Import the new TaskItem component
 const TaskList = () => {
     const dispatch = useDispatch();
     const tasks = useSelector(state => state.tasks.tasks) || [];
+    const categories = useSelector(state => state.categories.categories) || [];
+    const [filter, setFilter] = useState("All");
+
+    const filteredTasks = filter === "All" 
+    ? tasks 
+    : tasks.filter(task => task.categories.includes(filter));
 
     useEffect(() => {
         dispatch(fetchTasks());
@@ -15,7 +21,21 @@ const TaskList = () => {
 
     return (
         <div className="list-group mt-3">
-            {tasks && tasks.length > 0 ? tasks.map(task => (
+
+            {/* Dropdown filter */}
+            <select value={filter} onChange={(e) => setFilter(e.target.value)}>
+                <option key="All" value="All">
+                    All
+                </option>
+                {categories.map(category => (
+                    <option key={category.id} value={category.id}>
+                        {category.title}
+                    </option>
+                ))}
+            </select>
+
+
+            {filteredTasks && filteredTasks.length > 0 ? filteredTasks.map(task => (
                 <TaskItem key={task.id} task={task} /> 
             )): 'No Tasks Available.'}
         </div>
