7f359d2f63dba9c03356af3a3105f6e0d3c1a0d6
fcmdias <fernando.cesardias7@yahoo.com>
2023-10-01 22:55:15 +0100

    changed the selection of category in the task list for easier section

--- react-app/src/features/tasks/List.js
+++ react-app/src/features/tasks/List.js
@@ -1,16 +1,14 @@
-import React, {useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchTasks, setCategoryFilter } from './actions';
 import { fetchCategories } from '../categories/actions';
-import TaskItem from './Item';  // Import the new TaskItem component
+import TaskItem from './Item';
 
 const TaskList = () => {
     const dispatch = useDispatch();
-    // const tasks = useSelector(state => state.tasks.tasks) || [];
     const filteredTasks = useSelector(state => state.tasks.filteredTasks) || [];
     const categoryFilter = useSelector(state => state.tasks.categoryFilter) || "All";
     const categories = useSelector(state => state.categories.categories) || [];
-
 
     useEffect(() => {
         dispatch(fetchTasks());
@@ -19,18 +17,21 @@ const TaskList = () => {
 
     return (
         <div className="list-group mt-3">
-
-            <select value={categoryFilter} onChange={(e) => dispatch(setCategoryFilter(e.target.value))}>
-                <option key="All" value="All">
-                    All
-                </option>
-                {categories.map(category => (
-                    <option key={category.id} value={category.id}>
-                        {category.title}
-                    </option>
-                ))}
-            </select>
-
+            <button
+                className={`btn ${categoryFilter === 'All' ? 'btn-dark' : 'btn-outline-dark'} w-100 mb-2`}
+                onClick={() => dispatch(setCategoryFilter('All'))}
+            >
+                All
+            </button>
+            {categories.map(category => (
+                <button 
+                    key={category.id} 
+                    className={`btn ${categoryFilter === category.id ? 'btn-dark' : 'btn-outline-dark'} w-100 mb-2`}
+                    onClick={() => dispatch(setCategoryFilter(category.id))}
+                >
+                    {category.title}
+                </button>
+            ))}
 
             {filteredTasks && filteredTasks.length > 0 ? filteredTasks.map(task => (
                 <TaskItem key={task.id} task={task} /> 
